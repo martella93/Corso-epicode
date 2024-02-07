06ab0daf81ae2624024f69ba6ea97485554b6819
martella93 <157278234+martella93@users.noreply.github.com>
2024-02-08 00:14:04 +0100

    settimana 3

---
+++ Settimana3/assets/js/toDoList.js
@@ -0,0 +1,43 @@
+const task = document.getElementById('task');
+const btnAdd = document.getElementById("inserisci");
+const lista = document.getElementById("lista");
+let singleTask;
+let tasks = [];
+
+btnAdd.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (task.value === '') {
+        alert('inserire un task');
+        return;
+    }
+    carica();
+    scrivoLista();
+    cancellaForm();
+});
+
+
+const carica = () => {
+    singleTask = task.value;
+    tasks.push(singleTask);
+}
+
+const scrivoLista = () => {
+    lista.innerHTML = '';
+    tasks.forEach((element, index) => {
+        lista.innerHTML += `<li>${element}&nbsp;<button type="button" onclick="elimina(${index});">❌</button></li>`
+
+    });
+    cancellaForm();
+
+}
+
+
+function cancellaForm() {
+    task.value = '';
+}
+
+
+const elimina = (indice) => {
+    tasks.splice(indice, 1);
+    scrivoLista();
+}

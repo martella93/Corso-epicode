24b56f5d732dbc7d8fd22c46f92ccbe0cb02b777
martella93 <157278234+martella93@users.noreply.github.com>
2024-03-11 10:08:39 +0100

    progetto settimanale

--- UNIT2/Settimana3/Progetto_Settimanale/assets/js/script.js
+++ UNIT2/Settimana3/Progetto_Settimanale/assets/js/script.js
@@ -3,7 +3,7 @@ const API_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWVhZDU5ZTJkN2IxMTAwMTkwZTZkY2QiLCJpYXQiOjE3MDk4ODg5MjYsImV4cCI6MTcxMTA5ODUyNn0.5cMzW148MllRx_wsriGeodbK_f-7X0c-RA0BOzeHaqk";
 lista = [];
 const cards = document.getElementById("cards");
-const fetchProducts = async () => {
+const loadProducts = async () => {
   try {
     const response = await fetch(dataURL, {
       headers: {
@@ -23,58 +23,28 @@ const fetchProducts = async () => {
   }
 };
 
-const createCards = () => {
-  cards.innerHTML = "";
-  lista.forEach((item) => {
-    const card = document.createElement("div");
-    card.classList.add("card", "ms-3");
 
-    const immagine = document.createElement("img");
-    immagine.src = product.imageUrl;
-    immagine.alt = product.name;
-    card.appendChild(imageUrl);
-
-    const cardBody = document.createElement("div");
-    cardBody.classList.add("card-body");
-
-    const title = document.createElement("h3");
-    title.classList.add("card-title");
-    title.textContent = product.name;
-    cardBody.appendChild(title);
-
-    const brand = document.createElement("p");
-    brand.classList.add("card-text");
-    brand.textContent = `Brand: ${product.brand}`;
-    cardBody.appendChild(brand);
-
-    const price = document.createElement("p");
-    price.classList.add("card-text");
-    price.textContent = `Price: $${product.price}`;
-    cardBody.appendChild(price);
-
-    const description = document.createElement("p");
-    description.classList.add("card-text");
-    description.textContent = product.description;
-    cardBody.appendChild(description);
-
-    const editButton = document.createElement("button");
-    editButton.textContent = "Modifica";
-    editButton.classList.add("btn", "btn-success", "me-2");
-    editButton.addEventListener("click", () => {
-      window.location.href = `edit.html?id=${product._id}`;
-    });
-    cardBody.appendChild(editButton);
-
-    const detailsButton = document.createElement("button");
-    detailsButton.textContent = "Scopri di più";
-    detailsButton.classList.add("btn", "btn-primary", "me-2");
-    detailsButton.addEventListener("click", () => {
-        window.location.href = "details.html";
-    });
-    cardBody.appendChild(detailsButton);
-
-    card.appendChild(cardBody);
-
-  });
+  
+const createCardsProducts = (products) => {
+  let productsHTML = "";
+  for (let i = 0; i < products.length; i++) {
+    const product = productsArray[i];
+    productsHTML += `
+            <div class="col">
+                <div class="card">
+                    <img src="${product.imageUrl}" class="card-img-top" alt="Foto ${product.name}" />
+                    <div class="card-body">
+                        <h5 class="card-title">${product.name}</h5>
+                        <p class="card-text">${product.description}</p>
+                        <a class="btn btn-warning me-2 mb-2" href="edit.html?id=${product._id}">Modifica</a>
+                        <a class="btn btn-info" href="details.html?id=${product._id}">Scopri di più</a>
+                    </div>
+                </div>
+            </div>`;
+  }
+  productsList.innerHTML = productsHTML;
 };
+
+
+
 createCards();
